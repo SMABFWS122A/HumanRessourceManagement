d28ea36cee40e23ffecc3ab56963e8ec25cd7147
Lukas <92077102+StraussLukas@users.noreply.github.com>
2023-10-11 22:07:59 +0200

    created und uploaded initial spring boot project
    Spring web dependency already included;
    Java maven project;
    Java version: 17

---
+++ src/main/java/com/smabfws122a/humanressourcemanagement/HumanressourcemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.smabfws122a.humanressourcemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanressourcemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanressourcemanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smabfws122a/humanressourcemanagement/HumanressourcemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smabfws122a.humanressourcemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanressourcemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

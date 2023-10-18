cef9992decfafd1e1a2c7f8dfc8e9cd0ca772047
lukas <lukas.strauss1404@gmail.com>
2023-10-18 21:41:50 +0200

    flyway script created: added some initial data in table beschaeftigungsgrad

---
+++ src/main/resources/db/migration/V1_1__beschaeftigungsgrad_init_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO beschaeftigungsgrad(id, bezeichnung, wochenstunden, beginn_arbeitszeitfenster, ende_arbeitszeitfenster)
+VALUES (1, 'Vollzeit', 40.0, '7:00', '20:00');
+
+INSERT INTO beschaeftigungsgrad(id, bezeichnung, wochenstunden, beginn_arbeitszeitfenster, ende_arbeitszeitfenster)
+VALUES (2, 'Teilzeit', 30.0, '7:00', '20:00');
+
+INSERT INTO beschaeftigungsgrad(id, bezeichnung, wochenstunden, beginn_arbeitszeitfenster, ende_arbeitszeitfenster)
+VALUES (3, 'Azubi', 38.0, '7:00', '18:00');
+
+INSERT INTO beschaeftigungsgrad(id, bezeichnung, wochenstunden, beginn_arbeitszeitfenster, ende_arbeitszeitfenster)
+VALUES (4, 'Werkstudent', 20.0, '7:00', '18:00');
+

a6ba2e55c47a2468c6f3ebbd95bb808aff20f1aa
Valentino <valentino.pique@edu.fhdw.de>
2023-10-23 13:51:19 +0200

    Table 'Mitarbeiter' created an initial data edit

---
+++ src/main/resources/db/migration/V2_1__mitarbeiter_init_data.sql
@@ -0,0 +1,15 @@
+INSERT INTO mitarbeiter(personalnummer, vorname, nachname, email, beschaeftigungsgrad_id)
+VALUES (100, 'Hans', 'Müller', 'hansmueller@mail.de', 1);
+
+INSERT INTO mitarbeiter(personalnummer, vorname, nachname, email, beschaeftigungsgrad_id)
+VALUES (200, 'Monika', 'Schmitz', 'monikaschmitz@mail.de', 1);
+
+INSERT INTO mitarbeiter(personalnummer, vorname, nachname, email, beschaeftigungsgrad_id)
+VALUES (300, 'Leon', 'Neumann', 'lenoneumann@mail.de', 3);
+
+INSERT INTO mitarbeiter(personalnummer, vorname, nachname, email, beschaeftigungsgrad_id)
+VALUES (400, 'Sara', 'Musterfrau', 'saramustefrau@mail.de', 2);
+
+INSERT INTO mitarbeiter(personalnummer, vorname, nachname, email, beschaeftigungsgrad_id)
+VALUES (500, 'David', 'Mustermann', 'davidmustermann@mail.de', 4);
+

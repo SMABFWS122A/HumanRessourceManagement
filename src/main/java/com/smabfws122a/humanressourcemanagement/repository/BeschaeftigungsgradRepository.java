a941350bb5ae7dd7f8f1671a1bcda7e230dbfac8
Justus <justus_paul.stein@edu.fhdw.de>
2023-10-23 14:38:22 +0200

    Repository for added Entities added

---
+++ src/main/java/com/smabfws122a/humanressourcemanagement/repository/BeschaeftigungsgradRepository.java
@@ -0,0 +1,9 @@
+package com.smabfws122a.humanressourcemanagement.repository;
+
+import com.smabfws122a.humanressourcemanagement.entity.Beschaeftigungsgrad;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BeschaeftigungsgradRepository extends JpaRepository<Beschaeftigungsgrad, Integer> {
+
+
+}

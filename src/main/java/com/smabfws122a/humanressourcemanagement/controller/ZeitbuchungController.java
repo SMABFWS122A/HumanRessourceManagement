85158cff6aa6bca55fd2b972bb15f252d745840f
lukas <lukas.strauss1404@gmail.com>
2023-10-23 17:05:28 +0200

    ZeitbuchungService and ZeitbuchungController created. All endpoints for "Zeitbuchung" tested manually.

---
+++ src/main/java/com/smabfws122a/humanressourcemanagement/controller/ZeitbuchungController.java
@@ -0,0 +1,40 @@
+package com.smabfws122a.humanressourcemanagement.controller;
+
+import com.smabfws122a.humanressourcemanagement.entity.Zeitbuchung;
+import com.smabfws122a.humanressourcemanagement.service.ZeitbuchungService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ZeitbuchungController {
+
+    @Autowired
+    ZeitbuchungService service;
+
+    @PostMapping(value = "/zeitbuchung")
+    public Integer addZeitbuchung(@RequestBody Zeitbuchung zeitbuchung){
+        return service.addZeitbuchung(zeitbuchung);
+    }
+
+    @GetMapping(value = "/zeitbuchungen")
+    public List<Zeitbuchung> getAllZeitbuchungen(){
+        return service.getAllZeitbuchungen();
+    }
+
+    @GetMapping(value = "/zeitbuchung/{id}")
+    public Zeitbuchung getZeitbuchungById(@PathVariable Integer id){
+        return service.getZeitbuchungById(id);
+    }
+
+    @PutMapping(value = "/zeitbuchung")
+    public Integer updateZeitbuchung(@RequestBody Zeitbuchung zeitbuchung){
+        return service.updateZeitbuchung(zeitbuchung);
+    }
+
+    @DeleteMapping(value = "/zeibuchung/{id}")
+    public void deleteZeitbuchundByIs(@PathVariable Integer id){
+        service.deleteZeitbuchungById(id);
+    }
+}

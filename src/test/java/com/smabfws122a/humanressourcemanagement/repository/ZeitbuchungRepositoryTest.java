643abf5220c47cad4b4ee8be0ff831638c2af990
lukas <lukas.strauss1404@gmail.com>
2023-10-23 17:03:31 +0200

    ZeitbuchungRepositoryTest and MitarbeiterRepositoryTest created

---
+++ src/test/java/com/smabfws122a/humanressourcemanagement/repository/ZeitbuchungRepositoryTest.java
@@ -0,0 +1,91 @@
+package com.smabfws122a.humanressourcemanagement.repository;
+
+import com.smabfws122a.humanressourcemanagement.entity.Zeitbuchung;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.sql.Date;
+import java.sql.Time;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@ActiveProfiles("jpa-test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ZeitbuchungRepositoryTest {
+
+    @Autowired
+    ZeitbuchungRepository repository;
+
+    Zeitbuchung zeitbuchungKommen = new Zeitbuchung();
+    Zeitbuchung zeitbuchungGehen = new Zeitbuchung();
+
+    @BeforeAll
+    void setUp(){
+
+        zeitbuchungKommen.setUhrzeit(Time.valueOf("14:52:00"));
+        zeitbuchungKommen.setDatum(Date.valueOf("2023-10-23"));
+        zeitbuchungKommen.setBuchungsart("kommen");
+        zeitbuchungKommen.setPersonalnummer(100);
+        repository.saveAndFlush(zeitbuchungKommen);
+
+        zeitbuchungGehen.setUhrzeit(Time.valueOf("19:52:00"));
+        zeitbuchungGehen.setDatum(Date.valueOf("2023-10-23"));
+        zeitbuchungGehen.setBuchungsart("gehen");
+        zeitbuchungGehen.setPersonalnummer(100);
+        repository.saveAndFlush(zeitbuchungGehen);
+    }
+
+    @Test
+    void smokeTest(){ assertThat(repository).isNotNull(); }
+
+    @Test
+    void findAll_whenFindAll_thenResultHasSize2(){
+        // arrange
+        var expectedSize = 2;
+        // act
+        var actual = repository.findAll();
+        // assert
+        assertThat(actual).hasSize(expectedSize);
+    }
+
+    @Test
+    void findById_whenFound_thenReturnEntity(){
+        // act
+        var actual = repository.findById(1);
+        // assert
+        assertThat(actual.get().getId()).isEqualTo(1);
+        assertThat(actual.get().getUhrzeit()).isEqualTo(Time.valueOf("14:52:00"));
+        assertThat(actual.get().getDatum()).isEqualTo(Date.valueOf("2023-10-23"));
+        assertThat(actual.get().getBuchungsart()).isEqualTo("kommen");
+        assertThat(actual.get().getPersonalnummer()).isEqualTo(100);
+    }
+
+    @Test
+    void update_whenSuccesfull_thenEntityHasNewUhrzeit(){
+        // arrange
+        var zeitbuchungUpdate = new Zeitbuchung();
+        zeitbuchungUpdate.setId(1);
+        zeitbuchungUpdate.setUhrzeit(Time.valueOf("15:52:00"));
+        zeitbuchungUpdate.setDatum(Date.valueOf("2023-10-23"));
+        zeitbuchungUpdate.setBuchungsart("kommen");
+        zeitbuchungUpdate.setPersonalnummer(100);
+        // act
+        repository.save(zeitbuchungUpdate);
+        var actual = repository.findById(1);
+        // assert
+        assertThat(actual.get().getId()).isEqualTo(1);
+        assertThat(actual.get().getUhrzeit()).isEqualTo(Time.valueOf("15:52:00"));
+        assertThat(actual.get().getDatum()).isEqualTo(Date.valueOf("2023-10-23"));
+        assertThat(actual.get().getBuchungsart()).isEqualTo("kommen");
+        assertThat(actual.get().getPersonalnummer()).isEqualTo(100);
+    }
+
+    @AfterAll
+    void tearTown(){
+        repository.deleteAll();
+    }
+
+}

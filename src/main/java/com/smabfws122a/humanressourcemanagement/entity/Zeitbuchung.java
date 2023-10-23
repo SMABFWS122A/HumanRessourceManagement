b2352db4e03f17ce55b83df48bd1aad54948bd66
lukas <lukas.strauss1404@gmail.com>
2023-10-23 17:02:56 +0200

    Automatically generated id added.

--- src/main/java/com/smabfws122a/humanressourcemanagement/entity/Zeitbuchung.java
+++ src/main/java/com/smabfws122a/humanressourcemanagement/entity/Zeitbuchung.java
@@ -3,6 +3,7 @@ package com.smabfws122a.humanressourcemanagement.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.sql.Date;
 import java.sql.Time;
@@ -14,7 +15,16 @@ import java.sql.Time;
 public class Zeitbuchung {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(generator = "zeitbuchung-generator")
+    @GenericGenerator(
+            name = "zeitbuchung-generator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @org.hibernate.annotations.Parameter(name = "sequence_name", value = "zeitbuchung_id_seq"),
+                    @org.hibernate.annotations.Parameter(name = "initial_value", value = "1"),
+                    @org.hibernate.annotations.Parameter(name = "increment_size", value = "1")
+            }
+    )
     private Integer id;
     private Time uhrzeit;
     private Date datum;

7b8c9bee124b62c95658be2736052287f8954e0b
lukas <lukas.strauss1404@gmail.com>
2023-10-23 17:02:02 +0200

    Removed @Setter

--- src/main/java/com/smabfws122a/humanressourcemanagement/entity/Beschaeftigungsgrad.java
+++ src/main/java/com/smabfws122a/humanressourcemanagement/entity/Beschaeftigungsgrad.java
@@ -4,14 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.sql.Time;
 
 @Entity
 @Table(name = "beschaeftigungsgrad")
 @Getter
-@Setter
 public class Beschaeftigungsgrad {
     @Id
     private Integer id;

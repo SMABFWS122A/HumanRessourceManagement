a29193f0b5fbb0a89297230a6193281d534f411e
Valentino <valentino.pique@edu.fhdw.de>
2023-10-26 11:40:38 +0200

    Table 'Login' Controller created

---
+++ src/main/java/com/smabfws122a/humanressourcemanagement/controller/LoginController.java
@@ -0,0 +1,41 @@
+package com.smabfws122a.humanressourcemanagement.controller;
+
+import com.smabfws122a.humanressourcemanagement.entity.Login;
+import com.smabfws122a.humanressourcemanagement.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class LoginController {
+
+    @Autowired
+    LoginService service;
+
+    @PostMapping(value = "/login")
+    public String addLogin(@RequestBody Login login){
+        return service.addLogin(login);
+    }
+
+    @GetMapping(value = "/login")
+    public List<Login> getAllLogin(){
+        return service.getAllLogin();
+    }
+
+    @GetMapping(value = "/login/{email}")
+    public Login getLoginbyEmail(@PathVariable String email){
+        return service.getLoginbyEmail(email);
+    }
+
+    @PutMapping(value = "/login")
+    public String updateLogin(@RequestBody Login login){
+        return service.updateLogin(login);
+    }
+
+    @DeleteMapping(value = "/login/{email}")
+    public void deleteLoginbyEmail(@PathVariable String email){
+        service.deleteLoginbyEmail(email);
+    }
+}
+

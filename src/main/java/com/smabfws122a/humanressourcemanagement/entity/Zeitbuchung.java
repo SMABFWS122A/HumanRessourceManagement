ce0dda4c544e78f462c5661bfd6e144c96e52734
Valentino <valentino.pique@edu.fhdw.de>
2023-10-26 12:40:04 +0200

    Added strategy to entity 'Zeitbuchung'

--- src/main/java/com/smabfws122a/humanressourcemanagement/entity/Zeitbuchung.java
+++ src/main/java/com/smabfws122a/humanressourcemanagement/entity/Zeitbuchung.java
@@ -17,6 +17,7 @@ public class Zeitbuchung {
     @GeneratedValue(generator = "zeitbuchung-generator")
     @GenericGenerator(
             name = "zeitbuchung-generator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
             parameters = {
                     @org.hibernate.annotations.Parameter(name = "sequence_name", value = "zeitbuchung_id_seq"),
                     @org.hibernate.annotations.Parameter(name = "initial_value", value = "1"),

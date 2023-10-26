ae0bbed1a1ec90925c6075859a2e0c224e89e888
Valentino <valentino.pique@edu.fhdw.de>
2023-10-26 11:50:16 +0200

    Table 'Login' Entity Boolean to Bit converter deleted (doesnt't work)

--- src/main/java/com/smabfws122a/humanressourcemanagement/entity/Login.java
+++ src/main/java/com/smabfws122a/humanressourcemanagement/entity/Login.java
@@ -1,12 +1,10 @@
 package com.smabfws122a.humanressourcemanagement.entity;
 
-import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.type.NumericBooleanConverter;
 
 @Entity
 @Table(name = "login")
@@ -16,8 +14,7 @@ public class Login {
     @Id
     private String email;
     private String passwort;
-    @Convert(converter = NumericBooleanConverter.class)
-    private Boolean admin;
+    private Integer admin;  //nur 1 oder 0 für true oder false
     private Integer peronalnummmer;
 
 }

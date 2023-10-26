d5b480ede9811aea4bbe775b857aa1b91c53c4ab
Valentino <valentino.pique@edu.fhdw.de>
2023-10-26 11:46:19 +0200

    Table 'Login' Entity Boolean to Bit converter added

--- src/main/java/com/smabfws122a/humanressourcemanagement/entity/Login.java
+++ src/main/java/com/smabfws122a/humanressourcemanagement/entity/Login.java
@@ -1,10 +1,12 @@
 package com.smabfws122a.humanressourcemanagement.entity;
 
+import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.type.NumericBooleanConverter;
 
 @Entity
 @Table(name = "login")
@@ -14,7 +16,8 @@ public class Login {
     @Id
     private String email;
     private String passwort;
-    private Integer admin; //nur 1 oder 0
+    @Convert(converter = NumericBooleanConverter.class)
+    private Boolean admin;
     private Integer peronalnummmer;
 
 }

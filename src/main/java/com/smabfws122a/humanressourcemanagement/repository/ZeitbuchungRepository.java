66838fc563560369a86b7bc2d1fd8614e3d75a9a
lukas <lukas.strauss1404@gmail.com>
2023-10-31 18:18:13 +0100

    added 'findAllByPersonalnummerAndDatum'-methode to be able to calculate the gleitzeitsaldo

--- src/main/java/com/smabfws122a/humanressourcemanagement/repository/ZeitbuchungRepository.java
+++ src/main/java/com/smabfws122a/humanressourcemanagement/repository/ZeitbuchungRepository.java
@@ -3,5 +3,9 @@ package com.smabfws122a.humanressourcemanagement.repository;
 import com.smabfws122a.humanressourcemanagement.entity.Zeitbuchung;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.sql.Date;
+import java.util.List;
+
 public interface ZeitbuchungRepository extends JpaRepository<Zeitbuchung, Integer> {
+    List<Zeitbuchung> findAllByPersonalnummerAndDatum(Integer personalnumemr, Date datum);
 }
